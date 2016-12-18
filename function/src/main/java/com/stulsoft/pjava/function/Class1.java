63dd3bb0616aaa8aef547049156626b3c2ae03e0
ysden123 <ysden123@gmail.com>
2016-12-18 20:34:14 +0200

    generic static methods

---
+++ function/src/main/java/com/stulsoft/pjava/function/Class1.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016. Yuriy Stul
+ */
+
+package com.stulsoft.pjava.function;
+
+/**
+ * Base class for testing generic static methods
+ *
+ * @author Yuriy Stul
+ */
+class Class1 {
+    String getName(){
+        return "name1";
+    }
+}
