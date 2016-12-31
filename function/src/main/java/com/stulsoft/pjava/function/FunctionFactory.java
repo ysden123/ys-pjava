0c67bbd92f0d4d30b536b6192a2430c1af7f7dcc
ysden123 <ysden123@gmail.com>
2016-12-31 15:21:49 +0200

    return a function

---
+++ function/src/main/java/com/stulsoft/pjava/function/FunctionFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016. Yuriy Stul
+ */
+
+package com.stulsoft.pjava.function;
+
+import java.util.function.Consumer;
+
+/**
+ * Demonstrates the usage of function as return value.
+ *
+ * @author Yuriy Stul
+ */
+public class FunctionFactory {
+
+    public static void main(String[] args) {
+        System.out.println("==>main");
+        Consumer<Integer> f;
+
+        FunctionFactory ff = new FunctionFactory();
+        f = ff.getFunction(3);
+        f.accept(10);
+
+        f = ff.getFunction(30);
+        f.accept(10);
+
+        System.out.println("<==main");
+    }
+
+    /**
+     * Returns a function depending on input argument
+     *
+     * @param i input argument
+     * @return the function depending on input argument
+     */
+    private Consumer<Integer> getFunction(int i) {
+        Consumer<Integer> f;
+        if (i < 5) {
+            f = x -> System.out.println("Result is " + (x * 15));
+        } else {
+            f = x -> System.out.println("Result is " + (x + 15));
+        }
+
+        return f;
+    }
+}
