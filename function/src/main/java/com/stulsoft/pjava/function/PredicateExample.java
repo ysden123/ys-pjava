da2f77c5cbc4931727201eb94f6618129be550d6
Yuriy Stul <yuriy.stul@williamhill.com>
2017-01-26 14:33:03 +0200

    Usage of Predicate function

---
+++ function/src/main/java/com/stulsoft/pjava/function/PredicateExample.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017, William Hill Online. All rights reserved
+ */
+package com.stulsoft.pjava.function;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+/**
+ * Usage of Predicate function.
+ *
+ * @author Yuriy Stul.
+ */
+public class PredicateExample {
+	public static void main(String[] args) {
+		System.out.println("==>main");
+		Predicate<String> p = x -> x.equals("abc");
+		Arrays.asList("cccc", "dddd", "abc")
+				.stream()
+				.filter(p)
+				.forEach(System.out::println);
+		System.out.println("<==main");
+
+	}
+}
