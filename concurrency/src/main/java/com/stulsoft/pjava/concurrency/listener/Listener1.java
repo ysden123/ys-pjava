13370994145422813513bd64b0e9e7ea1654ac28
ysden123 <ysden123@gmail.com>
2017-05-26 15:13:11 +0300

    Listener1

---
+++ concurrency/src/main/java/com/stulsoft/pjava/concurrency/listener/Listener1.java
@@ -0,0 +1,97 @@
+package com.stulsoft.pjava.concurrency.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Simple listener
+ *
+ * @author Yuriy Stul
+ */
+public class Listener1 {
+    class Status {
+        String status;
+
+        String getStatus() {
+            return status;
+        }
+
+        void setStatus(String status) {
+            this.status = status;
+        }
+    }
+
+    private static Logger logger = LoggerFactory.getLogger(Listener1.class);
+    //    private final Object syncObject = new Object();
+    private final Status status = new Status();
+
+    /**
+     * Does a work
+     *
+     * Waits no more than 'timeout' milliseconds and checks status.
+     * If status is "Completed" then everything is OK; otherwise throws TimeoutException.
+     *
+     * @param timeout max time to wait in milliseconds
+     * @throws TimeoutException status was not set to Completed
+     */
+    private void work(long timeout) throws TimeoutException {
+        try {
+            logger.info("Waiting {} milliseconds...", timeout);
+            status.setStatus("Running");
+            synchronized (status) {
+                status.wait(timeout);
+                if ("Running".equals(status.getStatus()))
+                    throw new TimeoutException("Not completed yet...");
+            }
+            logger.info("Completed");
+        } catch (InterruptedException e) {
+            logger.error("Interrupted!");
+            throw new TimeoutException("Was interrupted");
+        }
+    }
+
+    /**
+     * Sets the status to 'Completed' and notifies the status object.
+     */
+    private void receiveMessage() {
+        logger.info("notify()");
+        synchronized (status) {
+            status.setStatus("Completed");
+            status.notify();
+        }
+    }
+
+    private static void test(long timeout, long delay){
+        logger.info("==>test. timeout={}, delay={}",timeout,delay);
+        Listener1 listener = new Listener1();
+        ExecutorService es = Executors.newCachedThreadPool();
+        es.execute(() -> {
+            try {
+                listener.work(timeout);
+            } catch (TimeoutException e) {
+                logger.error("Timeout. Message: " + e.getMessage(), e);
+            }
+        });
+
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            logger.error("(2) Interrupted");
+        }
+        listener.receiveMessage();
+        es.shutdown();
+        logger.info("<==test");
+    }
+
+    public static void main(String[] args) {
+        logger.info("==>main");
+        test(1000,100);
+        logger.info("\n");
+        test(300,700);
+        logger.info("<==main");
+    }
+}
