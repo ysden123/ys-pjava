a1561fa85ed89182e7aef340236a080a9a5e0bf1
ysden123 <ysden123@gmail.com>
2017-12-26 23:56:44 +0200

    BenchmarkMaster

--- utils/src/main/java/com/stulsoft/pjava/utils/BenchmarkMaster.java
+++ utils/src/main/java/com/stulsoft/pjava/utils/BenchmarkMaster.java
@@ -4,8 +4,49 @@
 
 package com.stulsoft.pjava.utils;
 
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
 /**
  * @author Yuriy Stul
  */
-public interface BenchmarkMaster {
+public final class BenchmarkMaster {
+    private BenchmarkMaster() {
+    }
+
+    /**
+     * Executes collection of functions and returns collection of benchmark results
+     *
+     * @param samples specifies collection of a functions to execute; the function returns
+     *                {@code SimpleEntry<String,Object>} where key is a text description and value is a result
+     * @return collection of benchmarks
+     */
+    public static Collection<BenchmarkResult> execute(Collection<Supplier<AbstractMap.SimpleEntry<String, ?>>> samples) {
+        if (samples == null) throw new IllegalArgumentException("samples could not be null");
+        return samples.stream().map(sample -> {
+            long start = System.nanoTime();
+            AbstractMap.SimpleEntry<String, ?> sampleResult = sample.get();
+            long end = System.nanoTime();
+            return new BenchmarkResult(TimeUnit.NANOSECONDS.toMillis(end - start), sampleResult.getKey(), sampleResult.getValue());
+        }).collect(Collectors.toList());
+    }
+
+    public static Collection<BenchmarkResult> sortBenchmarkResultsByDurationAscending(Collection<BenchmarkResult> results) {
+        if (results == null) throw new IllegalArgumentException("results could not be null");
+        return sortBenchmarkResults(results, true);
+    }
+
+    public static Collection<BenchmarkResult> sortBenchmarkResultsByDurationDescending(Collection<BenchmarkResult> results) {
+        if (results == null) throw new IllegalArgumentException("results could not be null");
+        return sortBenchmarkResults(results, false);
+    }
+
+    private static Collection<BenchmarkResult> sortBenchmarkResults(Collection<BenchmarkResult> results, boolean isAscending) {
+        return isAscending ? results.stream().sorted(Comparator.comparingLong(BenchmarkResult::getDuration)).collect(Collectors.toList()) :
+                results.stream().sorted((r1, r2) -> Long.compare(r2.getDuration(), r1.getDuration())).collect(Collectors.toList());
+    }
 }
---
+++ utils/src/test/java/com/stulsoft/pjava/utils/BenchmarkMasterTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2017. Yuriy Stul
+ */
+
+package com.stulsoft.pjava.utils;
+
+import org.junit.Test;
+
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Yuriy Stul
+ */
+public class BenchmarkMasterTest {
+
+    private AbstractMap.SimpleEntry<String, Integer> test1() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ignore) {
+        }
+        return new AbstractMap.SimpleEntry<>("test 1", 123);
+    }
+
+    private AbstractMap.SimpleEntry<String, Integer> test2() {
+        try {
+            Thread.sleep(110);
+        } catch (InterruptedException ignore) {
+        }
+        return new AbstractMap.SimpleEntry<>("test 2", 1230);
+    }
+
+    private AbstractMap.SimpleEntry<String, Integer> test3() {
+        try {
+            Thread.sleep(120);
+        } catch (InterruptedException ignore) {
+        }
+        return new AbstractMap.SimpleEntry<>("test 3", 12300);
+    }
+
+    @Test
+    public void execute() {
+        Collection<BenchmarkResult> results = BenchmarkMaster.execute(Arrays.asList(this::test1, this::test1));
+        assertNotNull(results);
+        assertTrue(results.size() > 0);
+
+        results.stream().findFirst().ifPresent(r -> {
+            assertEquals("test 1", r.getText());
+            assertEquals(123, r.getValue());
+            long durationDif = Math.abs(r.getDuration() - 100);
+            assertTrue(durationDif <= 5);   // Diff is no more than 5%
+        });
+    }
+
+    @Test
+    public void sortBenchmarkResultsByDurationAscending() {
+        Collection<BenchmarkResult> results = BenchmarkMaster.sortBenchmarkResultsByDurationAscending(
+                BenchmarkMaster.execute(Arrays.asList(this::test1, this::test2, this::test3)));
+        assertNotNull(results);
+        assertEquals(3, results.size());
+
+        assertEquals("test 1", ((BenchmarkResult) results.toArray()[0]).getText());
+        assertEquals("test 2", ((BenchmarkResult) results.toArray()[1]).getText());
+        assertEquals("test 3", ((BenchmarkResult) results.toArray()[2]).getText());
+    }
+
+    @Test
+    public void sortBenchmarkResultsByDurationDescending() {
+        Collection<BenchmarkResult> results = BenchmarkMaster.sortBenchmarkResultsByDurationDescending(
+                BenchmarkMaster.execute(Arrays.asList(this::test1, this::test2, this::test3)));
+        assertNotNull(results);
+        assertEquals(3, results.size());
+
+        assertEquals("test 3", ((BenchmarkResult) results.toArray()[0]).getText());
+        assertEquals("test 2", ((BenchmarkResult) results.toArray()[1]).getText());
+        assertEquals("test 1", ((BenchmarkResult) results.toArray()[2]).getText());
+    }
+}
