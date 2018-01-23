35a562327efda26bada6da7ee5e6617719db4396
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2018-01-23 16:58:58 +0200

    abstract and interface

---
+++ basics/src/main/java/com/stulsoft/pjava/basics/interfaces/Abstract2Runner.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018, William Hill Online. All rights reserved
+ */
+package com.stulsoft.pjava.basics.interfaces;
+
+/**
+ * @author Yuriy Stul.
+ */
+public class Abstract2Runner {
+    public static void main(String[] args) {
+        Abstract2 a2 = new Abstract2() {
+            @Override
+            void foo() {
+                System.out.println("Fooooo");
+            }
+        };
+        a2.foo();
+
+        System.out.println(I1.n);
+
+        I1 i1 = new I1() {
+            @Override
+            public void foo2() {
+                System.out.println("foo2");
+            }
+        };
+        i1.foo1();
+    }
+}
---
+++ basics/src/main/java/com/stulsoft/pjava/basics/interfaces/I1.java
@@ -0,0 +1,10 @@
+package com.stulsoft.pjava.basics.interfaces;
+
+/**
+ * @author Yuriy Stul.
+ */
+public interface I1 {
+    int n = 123;
+    default void foo1(){System.out.println("foo1");}
+    void foo2();
+}
