6df82d68e84eb7e50c4588f3c1ae66ea87cfbe96
Yuriy Stul <ysden123@gmail.com>
2018-02-03 16:13:54 +0200

    wildcard

---
+++ basics/src/main/java/com/stulsoft/pjava/basics/generics/extention/A.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pjava.basics.generics.extention;
+
+/**
+ * @author Yuriy Stul
+ */
+public class A {
+    void foo1(){
+        System.out.println("==>foo1");
+    }
+}
---
+++ basics/src/main/java/com/stulsoft/pjava/basics/generics/extention/GenExt3.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pjava.basics.generics.extention;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Yuriy Stul
+ */
+public class GenExt3<T extends A> {
+    void useFoo1(List<? extends T> l) {
+        l.forEach(A::foo1);
+    }
+
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        GenExt3<A> genExt1 = new GenExt3<>();
+        genExt1.useFoo1(Arrays.asList(a, b));
+        GenExt3<B> genExt2 = new GenExt3<>();
+// error        genExt2.useFoo1(Arrays.asList(a, b));
+        genExt2.useFoo1(Arrays.asList(b));
+    }
+}
