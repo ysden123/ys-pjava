84681667fc0f4ccc48d8716799ec6ab8bfeef6c4
Yuriy Stul <yuriy.s@dauup-clicksmob.com>
2018-02-07 14:33:15 +0200

    HexToLong

---
+++ basics/src/main/java/com/stulsoft/pjava/basics/converts/HexToLong.java
@@ -0,0 +1,18 @@
+/*
+   Created by Yuriy Stul 2018
+*/
+package com.stulsoft.pjava.basics.converts;
+
+/**
+ * Converting Hexadecimal to long
+ */
+public class HexToLong {
+    public static void main(String[] args) {
+        System.out.println("==>main");
+        long l1 = 0xFFFFFFFFL;
+        System.out.println(l1);
+        long l2 = 0xFFFFFFFF;
+        System.out.println(l2);
+        System.out.println("<==main");
+    }
+}
