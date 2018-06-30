8334ffc0d50acd8373cad9605f74184baac77dc0
Yuriy Stul <ysden123@gmail.com>
2018-06-30 14:45:30 +0300

    finf first

---
+++ basics/src/main/java/com/stulsoft/pjava/basics/stream/FindFirst1.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pjava.basics.stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.stream.IntStream;
+
+/**
+ * Playing wit findFirst
+ *
+ * @author Yuriy Stul
+ */
+public class FindFirst1 {
+    private static final Logger logger = LoggerFactory.getLogger(FindFirst1.class);
+
+    public static void main(String[] args) {
+        logger.info("==>main");
+        f1();
+        logger.info("<==main");
+    }
+
+    private static void f1() {
+        logger.info("==>f1");
+        var result = IntStream.rangeClosed(1, 10).filter(i -> i == 5).findFirst();
+        logger.info("(1) result: {}", result);
+        result.ifPresent(i -> logger.info("(2) result (int) = {}", i));
+        logger.info("<==f1");
+    }
+}
