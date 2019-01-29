a3748e30f4a9dbabe9655f5685fd6a016f03fa84
Yuriy Stul <ysden123@gmail.com>
2019-01-29 20:17:09 +0200

    started 11

---
+++ java11/src/main/java/com/stulsoft/pjava/java11/collection/ToArray.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019. Yuriy Stul
+ */
+
+package com.stulsoft.pjava.java11.collection;
+
+import java.util.Arrays;
+
+/**
+ * @author Yuriy Stul
+ */
+public class ToArray {
+    public static void main(String[] args) {
+        System.out.println("==>main");
+        var collection = Arrays.asList("One", "Two", "Three");
+        var array = collection.toArray();
+        for (var i = 0; i < array.length; ++i) {
+            System.out.printf("array[%d]: %s%n", i, array[i]);
+        }
+        System.out.println("<==main");
+    }
+}
