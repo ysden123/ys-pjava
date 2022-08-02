f0abd9d3cc28ce8fbb442b5bb5dfb8a753b8c832
Yuriy Stul <yuriy.stul@whitesourcesoftware.com>
2022-08-02 13:25:34 +0300

    test substring

--- basics/src/main/java/com/stulsoft/pjava/basics/string/SubstringTest.java
+++ basics/src/main/java/com/stulsoft/pjava/basics/string/SubstringTest.java
@@ -1,4 +1,24 @@
-package com.stulsoft.pjava.basics.string;/**
+package com.stulsoft.pjava.basics.string;
+
+/**
  * @author Yuriy Stul
- **/public class SublistTest {
+ **/
+public class SubstringTest {
+    private static void test(String str, int maxLength){
+        if (str.length() > maxLength) {
+            str = str.substring(0, 2);
+        }
+        System.out.println("in test " + str);
+    }
+
+    public static void main(String[] args) {
+        String str;
+        str = "012";
+        test(str, 2);
+        System.out.println("after test " + str);
+
+        str="0";
+        test(str, 2);
+        System.out.println("after test " + str);
+    }
 }
