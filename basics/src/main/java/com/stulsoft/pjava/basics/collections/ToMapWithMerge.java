1ef96c4c022fa635e80d24ad85bfc4e08f030f15
Yuriy Stul <yuriy.stul@whitesourcesoftware.com>
2022-09-07 11:30:31 +0300

    Collector toMap with merging

---
+++ basics/src/main/java/com/stulsoft/pjava/basics/collections/ToMapWithMerge.java
@@ -0,0 +1,66 @@
+package com.stulsoft.pjava.basics.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author Yuriy Stul
+ **/
+public class ToMapWithMerge {
+    static class Container {
+        private final Long id;
+        private final String name;
+
+        public Container(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "Container{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("==>main");
+        test1();
+        test2();
+    }
+
+    private static void test1() {
+        System.out.println("==>test1");
+        List<Container> list = new ArrayList<>();
+        list.add(new Container(1L, "n1"));
+        list.add(new Container(1L, "n2"));
+        list.add(new Container(2L, "n3"));
+        Map<Long, Container> map = list.stream().collect(Collectors.toMap(Container::getId, Function.identity(), (p1, p2) -> p1));
+        System.out.println("list: " + list);
+        System.out.println("map: " + map);
+    }
+
+    private static void test2() {
+        System.out.println("==>test2");
+        List<Container> list = new ArrayList<>();
+        list.add(new Container(1L, "n1"));
+        list.add(new Container(1L, "n2"));
+        list.add(new Container(2L, "n3"));
+        Map<Long, Container> map = list.stream().collect(Collectors.toMap(Container::getId, p -> p, (p1, p2) -> p1));
+        System.out.println("list: " + list);
+        System.out.println("map: " + map);
+    }
+}
