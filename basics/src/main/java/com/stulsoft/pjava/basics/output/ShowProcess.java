a34ec3db620485314dd25ef8d3e3ff85a47c31b5
Yuriy Stul <yuriy.stul@whitesourcesoftware.com>
2022-09-20 09:17:25 +0300

    output from the beginning of line

---
+++ basics/src/main/java/com/stulsoft/pjava/basics/output/ShowProcess.java
@@ -0,0 +1,11 @@
+package com.stulsoft.pjava.basics.output;
+
+/**
+ * @author Yuriy Stul
+ **/
+public class ShowProcess {
+    public static void main(String[] args) {
+        System.out.print("\rone");
+        System.out.print("\rtwo");
+    }
+}
